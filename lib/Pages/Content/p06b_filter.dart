67361df04f97e7f2390e0706d0aced1fcfcd28e2
Drakvor <drakvorvuramios@gmail.com>
2022-04-29 12:57:34 +0900

    Category Select

---
+++ lib/Pages/Content/p06b_filter.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class FilterOverlay extends StatelessWidget {
+  const FilterOverlay({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(),
+        Row(
+          children: [
+            Column(),
+            Column(),
+            Column(
+              children: [
+                Container(),
+                Row(
+                  children: [
+                    Column(),
+                    Column(),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+        Container(),
+      ],
+    );
+  }
+}
